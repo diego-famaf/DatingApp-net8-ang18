30b2110f430d3020573e8271d92ffe7c5cdbd865
diego-famaf <diego.linares@mi.unc.edu.ar>
2025-01-23 07:54:21 -0300

    End of Section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,8 +5,9 @@ namespace API.DTOs;
 
 public class RegisterDto
 {
-    [Required]
-    public required string Username { get; set; }
-    [Required]
-    public required string Password { get; set; }
+    [Required(ErrorMessage = "El nombre de usuario es requerido")]
+    public  string Username { get; set; } = string.Empty;
+    [Required(ErrorMessage = "La contraseña es requerida")]
+    [StringLength(8,MinimumLength =4,ErrorMessage = "La contraseña debe tener entre 4 y 8 caracteres.")]
+    public  string Password { get; set; } = string.Empty;
 }
